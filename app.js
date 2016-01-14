a39f321e71eaadfe0f9817db1f9f7dfb7db1af76
jorge <jcortesc@hotmail.com>
2016-01-13 20:55:50 -0800

    transfering the file daily

---
+++ app.js
@@ -0,0 +1,9 @@
+//my javascript code
+var firstname = prompt ('Your First Name','');
+var lastname = prompt ('Your Last Name','');
+
+console.log(firstname);
+
+alert(lastname);
+var r = confirm("Press a button!");
+alert("selected button was: "+r);
